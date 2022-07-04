3465894c1a1ec209c427a6f52460fc3669b523dd
YuigaWada <yuigawada@gmail.com>
2022-07-04 22:53:40 +0900

    [add] add video.py

---
+++ video.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+from tqdm import tqdm
+from main import get_argparser, anonymask
+
+
+def main():
+    parser = get_argparser()
+    parser.add_argument("--video_path", default="data/video.mp4")
+    args = parser.parse_args()
+
+    cap = cv.VideoCapture(args.video_path)
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    size = (width, height)
+    frame_rate = int(cap.get(cv.CAP_PROP_FPS))
+    frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
+    fmt = cv.VideoWriter_fourcc(*"MJPG")
+    writer = cv.VideoWriter('./output/video.avi', fmt, frame_rate, size)
+
+    ret = True
+    for i in tqdm(range(frame_count)):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = anonymask(args, frame)
+        frame[frame < 0] = 0
+        frame[frame > 255] = 255
+        writer.write(frame)
+
+    writer.release()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()
