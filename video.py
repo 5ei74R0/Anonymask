95c62c03bf38176341dea3cfbec9f3530f1e4500
YuigaWada <yuigawada@gmail.com>
2022-07-05 17:48:08 +0900

    [fix] make sure to work video.py

--- video.py
+++ video.py
@@ -1,12 +1,15 @@
 import cv2 as cv
 from tqdm import tqdm
-from main import get_argparser, anonymask
+from main import Anonymask
+from util.argparse import get_argparser, get_checkpoints
 
 
 def main():
     parser = get_argparser()
     parser.add_argument("--video_path", default="data/video.mp4")
     args = parser.parse_args()
+    checkpoints = get_checkpoints(args)
+    anonymask = Anonymask(checkpoints, device=args.device)
 
     cap = cv.VideoCapture(args.video_path)
     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -23,7 +26,7 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
-        frame = anonymask(args, frame)
+        frame = anonymask(frame)
         frame[frame < 0] = 0
         frame[frame > 255] = 255
         writer.write(frame)
